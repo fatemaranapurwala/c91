b4f3323465dbf1636342068ea77333ed0c6abfd6
fatemaranapurwala <143012337+fatemaranapurwala@users.noreply.github.com>
2023-11-22 21:03:35 +0530

    Add files via upload

---
+++ screen/shopping.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class shopping extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>shopping</Text>
+            </View>
+        )
+    }
+}
